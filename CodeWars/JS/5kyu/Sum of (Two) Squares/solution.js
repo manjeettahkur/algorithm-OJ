41c1a17e38bf0d9dd1657c84960fd4362bf6075d
Leo Hui <leohxj@gmail.com>
2015-10-12 10:03:19 +0800

    add new 5kyu

---
+++ CodeWars/JS/5kyu/Sum of (Two) Squares/solution.js
@@ -0,0 +1,6 @@
+function allSquaredPairs(num) {
+  // max(num) === 2147483647
+  
+  // Return every unique pair of numbers [a,b] where (a * a) + (b * b) = num;
+  // return value will be a two-dimensional array [[]]
+}
